817849f4fec151178c4ad96d22431aa3b87745c6
Dmitry Palaznik <dpalaznik@gmail.com>
2015-01-17 23:21:28 +0300

    Changed greating message

--- src/main/java/com/my/algorithms/week1/HelloAlgorithms.java
+++ src/main/java/com/my/algorithms/week1/HelloAlgorithms.java
@@ -2,6 +2,6 @@ package com.my.algorithms.week1;
 
 public class HelloAlgorithms {
     public static void main(String[] args) {
-        System.out.println("Hello, Algorithms!");
+        System.out.println("Hello, Algorithms!!!");
     }
 }
