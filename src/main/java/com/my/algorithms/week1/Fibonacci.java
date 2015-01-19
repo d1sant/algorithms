58445ac4edd63e928cca5850becf54ce000b0de1
Dmitry Palaznik <dpalaznik@gmail.com>
2015-01-19 10:12:23 +0300

    Simple implementation of fibonacci algorithm

---
+++ src/main/java/com/my/algorithms/week1/Fibonacci.java
@@ -0,0 +1,17 @@
+package com.my.algorithms.week1;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        System.out.println("Fib 0: " + fib(0));
+        System.out.println("Fib 1: " + fib(1));
+        System.out.println("Fib 2: " + fib(2));
+        System.out.println("Fib 3: " + fib(3));
+        System.out.println("Fib 4: " + fib(4));
+        System.out.println("Fib 5: " + fib(5));
+        // System.out.println("Fib 5: " + fib(100)); // too slow
+    }
+
+    public static long fib(int n) {
+        return n <= 1 ? n : fib(n - 1) + fib(n - 2);
+    }
+}
---
+++ src/main/java/com/my/algorithms/week1/Main.java
@@ -0,0 +1,16 @@
+package com.my.algorithms.week1;
+
+import java.util.Scanner;
+
+/**
+ * Introductory class to course from its creator.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+        int b = s.nextInt();
+        System.out.println(a + b);
+    }
+
+}
