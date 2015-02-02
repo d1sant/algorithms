1cf2ded80dda9aa49e60ee82fcdaa09bb03e3549
Dmitry Palaznik <dpalaznik@gmail.com>
2015-02-02 15:20:39 +0300

    Tree of segment: refactor of api and test with updated java doc

--- src/main/java/com/my/algorithms/week3/third/TreeOfSegments.java
+++ src/main/java/com/my/algorithms/week3/third/TreeOfSegments.java
@@ -2,50 +2,63 @@ package com.my.algorithms.week3.third;
 
 import java.util.Arrays;
 
+/**
+ * Tree of segment algorithm implementation.
+ */
 public class TreeOfSegments {
 
     private final int[] segmentTree;
-    private final int length;
+    private final int numberOfElements; // rounded value to nearest power of two
 
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
 
+        // rounding to nearest power of two
         System.out.println("3: " + round(3));
         System.out.println("6: " + round(6));
+        System.out.println("8: " + round(8));
         System.out.println("19: " + round(19));
         System.out.println("79: " + round(79));
         System.out.println("3445: " + round(3445));
         System.out.println("59994949: " + round(59994949) + "\n");
 
+        // power of two with left shift
         System.out.println(1 << 17);
         System.out.println(2 << 3);
         System.out.println(1 << 3);
-        System.out.println(3 << 1);
+        System.out.println((3 << 1) + "\n");
 
-        final int numberOfVal = 5;
-        final TreeOfSegments treeOfSegments = new TreeOfSegments(numberOfVal);
+        final TreeOfSegments treeOfSegments = new TreeOfSegments(5);
         final int[] values = {1, 2, 3, 4, 5};
-        final int numberOfValRounded = round(numberOfVal);
-        for (int i = 0; i < values.length; i++) {
-            treeOfSegments.set(1, 0, numberOfValRounded - 1, i, values[i]);
+        System.out.println("Values: " + Arrays.toString(values));
+        for (int index = 0; index < values.length; index++) {
+            treeOfSegments.set(index + 1, values[index]);
         }
 
         System.out.println("Set array: " + Arrays.toString(treeOfSegments.segmentTree));
 
-        System.out.println("Min(1,5): " + treeOfSegments.getMin(1, 0, numberOfValRounded - 1, 0, 4));
-        treeOfSegments.set(1, 0, numberOfValRounded - 1, 0, 8); // 1, 8
-        System.out.println("Min(1,5): " + treeOfSegments.getMin(1, 0, numberOfValRounded - 1, 0, 4));
-        System.out.println("Min(1,1): " + treeOfSegments.getMin(1, 0, numberOfValRounded - 1, 0, 0));
-        System.out.println("Min(1,3): " + treeOfSegments.getMin(1, 0, numberOfValRounded - 1, 0, 2));
-        treeOfSegments.set(1, 0, numberOfValRounded - 1, 2, 1); // 3, 1
-        System.out.println("Min(2,4): " + treeOfSegments.getMin(1, 0, numberOfValRounded - 1, 1, 3));
+        System.out.println("Min(1,5): " + treeOfSegments.getMin(1, 5));
+        treeOfSegments.set(1, 8);
+        System.out.println("Min(1,5): " + treeOfSegments.getMin(1, 5));
+        System.out.println("Min(1,1): " + treeOfSegments.getMin(1, 1));
+        System.out.println("Min(1,3): " + treeOfSegments.getMin(1, 3));
+        treeOfSegments.set(3, 1);
+        System.out.println("Min(2,4): " + treeOfSegments.getMin(2, 4) + "\n");
+
+        final TreeOfSegments fullTreeOfSegments = new TreeOfSegments(5);
+        final int[] fullValues = {5, 2, 7, 4, 4, 6, 8, 10};
+        System.out.println("Values: " + Arrays.toString(fullValues));
+        for (int index = 0; index < fullValues.length; index++) {
+            fullTreeOfSegments.set(index + 1, fullValues[index]);
+        }
+
+        System.out.println("Set array: " + Arrays.toString(fullTreeOfSegments.segmentTree));
     }
 
     public TreeOfSegments(final int elementNum) {
-        this.length = 2 * round(elementNum);
-        this.segmentTree = new int[length];
-        Arrays.fill(segmentTree, round(elementNum), length, Integer.MAX_VALUE);
-        System.out.println("Length: " + length);
-        System.out.println("Array: " + Arrays.toString(segmentTree));
+        numberOfElements = round(elementNum);
+        final int length = 2 * numberOfElements;
+        segmentTree = new int[length];
+        Arrays.fill(segmentTree, numberOfElements, length, Integer.MAX_VALUE);
     }
 
     public static int round(final int value) {
@@ -53,15 +66,25 @@ public class TreeOfSegments {
     }
 
     /**
-     * Sets value segment tree with calculation of minimum.
+     * Sets value to segment tree with calculation of minimum.
+     *
+     * @param index index of element. Indexes begin with 1
+     * @param value value of element
+     */
+    public void set(final int index, final int value) {
+        set(1, 0, numberOfElements - 1, index - 1, value);
+    }
+
+    /**
+     * Sets value to segment tree with calculation of minimum.
      *
      * @param vertex vertex
-     * @param vertexLeft left border of segment that corresponds to vertex vertex
-     * @param vertexRight right border of segment that corresponds to vertex vertex
+     * @param vertexLeft left index of sub array that corresponds to vertexes
+     * @param vertexRight right index of sub array that corresponds to vertexes
      * @param index index of cell
      * @param value value that will be placed in cell with index index
      */
-    public void set(final int vertex, final int vertexLeft, final int vertexRight, final int index, final int value) {
+    protected void set(final int vertex, final int vertexLeft, final int vertexRight, final int index, final int value) {
         if (index < vertexLeft || vertexRight < index) {
             return;
         }
@@ -78,14 +101,25 @@ public class TreeOfSegments {
     /**
      * Gets minimum on defined segment.
      *
-     * @param vertex vertex
-     * @param vertexLeft left border of segment that corresponds to vertex vertex
-     * @param vertexRight right border of segment that corresponds to vertex vertex
-     * @param left left border
-     * @param right right border
+     * @param left left index. Indexes begin with 1
+     * @param right right index. Indexes begin with 1
      * @return minimum on defined segment
      */
-    public int getMin(final int vertex, final int vertexLeft, final int vertexRight, final int left, final int right) {
+    public int getMin(final int left, final int right) {
+        return getMin(1, 0, numberOfElements - 1, left - 1, right - 1);
+    }
+
+    /**
+     * Gets minimum on defined segment.
+     *
+     * @param vertex vertex
+     * @param vertexLeft left index of sub array that corresponds to vertexes
+     * @param vertexRight right index of sub array that corresponds to vertexes
+     * @param left left index
+     * @param right right index
+     * @return minimum on defined segment
+     */
+    protected int getMin(final int vertex, final int vertexLeft, final int vertexRight, final int left, final int right) {
         if (right < vertexLeft || vertexRight < left) {
             return Integer.MAX_VALUE;
         }
