e41e358fcf337fa963c7631c8d25020fc30615ad
Dmitry Palaznik <dpalaznik@gmail.com>
2015-02-02 16:34:45 +0300

    Draft implementation of Non overlapping algorithm

---
+++ src/main/java/com/my/algorithms/week3/fourth/NonOverlappingSet.java
@@ -0,0 +1,27 @@
+package com.my.algorithms.week3.fourth;
+
+/**
+ * Non-overlapping set algorithm implementation.
+ *
+ * API:
+ * makeSet(x)
+ * union(x, y) unions sets that contain x and y
+ * find(x) return identifier of set that contains x
+ */
+public class NonOverlappingSet {
+
+    private NonOverlappingSet parent;
+    private int rank;
+
+    public NonOverlappingSet makeSet(final int val) {
+        return null; // TODO implement me
+    }
+
+    public int find(final int val) {
+        return -1; // TODO implement me
+    }
+
+    public NonOverlappingSet union(final int x, final int y) {
+        return null; // TODO implement me
+    }
+}
