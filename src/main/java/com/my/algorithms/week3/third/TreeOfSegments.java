7bad449aa4b73e4f9febe6a8bcea02e3a4ef4a21
Dmitry Palaznik <dpalaznik@gmail.com>
2015-02-02 14:40:01 +0300

    TreeOfSegments: some refactor and fixes

--- src/main/java/com/my/algorithms/week3/third/TreeOfSegments.java
+++ src/main/java/com/my/algorithms/week3/third/TreeOfSegments.java
@@ -1,5 +1,7 @@
 package com.my.algorithms.week3.third;
 
+import java.util.Arrays;
+
 public class TreeOfSegments {
 
     private final int[] segmentTree;
@@ -7,70 +9,92 @@ public class TreeOfSegments {
 
     public static void main(String[] args) {
 
-        System.out.println(round(3));
-        System.out.println(round(19));
-        System.out.println(round(79));
-        System.out.println(round(3445));
-        System.out.println(round(59994949) + "\n");
+        System.out.println("3: " + round(3));
+        System.out.println("6: " + round(6));
+        System.out.println("19: " + round(19));
+        System.out.println("79: " + round(79));
+        System.out.println("3445: " + round(3445));
+        System.out.println("59994949: " + round(59994949) + "\n");
 
         System.out.println(1 << 17);
         System.out.println(2 << 3);
         System.out.println(1 << 3);
         System.out.println(3 << 1);
+
+        final int numberOfVal = 5;
+        final TreeOfSegments treeOfSegments = new TreeOfSegments(numberOfVal);
+        final int[] values = {1, 2, 3, 4, 5};
+        final int numberOfValRounded = round(numberOfVal);
+        for (int i = 0; i < values.length; i++) {
+            treeOfSegments.set(1, 0, numberOfValRounded - 1, i, values[i]);
+        }
+
+        System.out.println("Set array: " + Arrays.toString(treeOfSegments.segmentTree));
+
+        System.out.println("Min(1,5): " + treeOfSegments.getMin(1, 0, numberOfValRounded - 1, 0, 4));
+        treeOfSegments.set(1, 0, numberOfValRounded - 1, 0, 8); // 1, 8
+        System.out.println("Min(1,5): " + treeOfSegments.getMin(1, 0, numberOfValRounded - 1, 0, 4));
+        System.out.println("Min(1,1): " + treeOfSegments.getMin(1, 0, numberOfValRounded - 1, 0, 0));
+        System.out.println("Min(1,3): " + treeOfSegments.getMin(1, 0, numberOfValRounded - 1, 0, 2));
+        treeOfSegments.set(1, 0, numberOfValRounded - 1, 2, 1); // 3, 1
+        System.out.println("Min(2,4): " + treeOfSegments.getMin(1, 0, numberOfValRounded - 1, 1, 3));
     }
 
     public TreeOfSegments(final int elementNum) {
         this.length = 2 * round(elementNum);
         this.segmentTree = new int[length];
+        Arrays.fill(segmentTree, round(elementNum), length, Integer.MAX_VALUE);
+        System.out.println("Length: " + length);
+        System.out.println("Array: " + Arrays.toString(segmentTree));
     }
 
     public static int round(final int value) {
-        return 2 << (value == 0 ? 0 : 32 - Integer.numberOfLeadingZeros(value - 1));
+        return 1 << (value == 0 ? 0 : 32 - Integer.numberOfLeadingZeros(value - 1));
     }
 
     /**
      * Sets value segment tree with calculation of minimum.
      *
-     * @param v vertex
-     * @param vl left border of segment that corresponds to vertex v
-     * @param vr right border of segment that corresponds to vertex v
-     * @param i index of cell
-     * @param x value that will be placed in cell with index i
+     * @param vertex vertex
+     * @param vertexLeft left border of segment that corresponds to vertex vertex
+     * @param vertexRight right border of segment that corresponds to vertex vertex
+     * @param index index of cell
+     * @param value value that will be placed in cell with index index
      */
-    public void setX(final int v, final int vl, final int vr, final int i, final int x) {
-        if (i < vl || vr < i) {
+    public void set(final int vertex, final int vertexLeft, final int vertexRight, final int index, final int value) {
+        if (index < vertexLeft || vertexRight < index) {
             return;
         }
-        if (vl == vr) {
-            segmentTree[v] = x;
+        if (vertexLeft == vertexRight) {
+            segmentTree[vertex] = value;
             return;
         }
-        int vm = (vl + vr) / 2;
-        setX(2 * v, vl, vm, i, x);
-        setX(2 * v + 1, vm + 1, vr, i, x);
-        segmentTree[v] = Math.min(segmentTree[v * 2], segmentTree[v * 2 + 1]);
+        int vertexMiddle = (vertexLeft + vertexRight) / 2;
+        set(2 * vertex, vertexLeft, vertexMiddle, index, value);
+        set(2 * vertex + 1, vertexMiddle + 1, vertexRight, index, value);
+        segmentTree[vertex] = Math.min(segmentTree[vertex * 2], segmentTree[vertex * 2 + 1]);
     }
 
     /**
      * Gets minimum on defined segment.
      *
-     * @param v vertex
-     * @param vl left border of segment that corresponds to vertex v
-     * @param vr right border of segment that corresponds to vertex v
-     * @param l left border
-     * @param r right border
+     * @param vertex vertex
+     * @param vertexLeft left border of segment that corresponds to vertex vertex
+     * @param vertexRight right border of segment that corresponds to vertex vertex
+     * @param left left border
+     * @param right right border
      * @return minimum on defined segment
      */
-    public int getMin(final int v, final int vl, final int vr, final int l, final int r) {
-        if (r < vl || vr < l) {
+    public int getMin(final int vertex, final int vertexLeft, final int vertexRight, final int left, final int right) {
+        if (right < vertexLeft || vertexRight < left) {
             return Integer.MAX_VALUE;
         }
-        if (l <= vl && vr <= r) {
-            return segmentTree[v];
+        if (left <= vertexLeft && vertexRight <= right) {
+            return segmentTree[vertex];
         }
-        final int vm = (vl + vr) / 2;
-        final int a = getMin(2 * v, vl, vm, l, r);
-        final int b = getMin(2 * v + 1, vm + 1, vr, l, r);
-        return Math.min(a, b);
+        final int vertexMiddle = (vertexLeft + vertexRight) / 2;
+        final int minLeft = getMin(2 * vertex, vertexLeft, vertexMiddle, left, right);
+        final int minRight = getMin(2 * vertex + 1, vertexMiddle + 1, vertexRight, left, right);
+        return Math.min(minLeft, minRight);
     }
 }
