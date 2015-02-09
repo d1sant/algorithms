cb4d76cd9907df48a3f906c8aa4a1b9ad1736e98
Dmitry Palaznik <dpalaznik@gmail.com>
2015-02-09 12:21:34 +0300

    Stable counting sort: some refinements

--- src/main/java/com/my/algorithms/week4/first/StableCountingSort.java
+++ src/main/java/com/my/algorithms/week4/first/StableCountingSort.java
@@ -8,11 +8,14 @@ import java.util.Arrays;
 public class StableCountingSort {
 
     public static void main(String[] args) {
+
         final Integer[] unsorted = {new Integer(2), new Integer(3), new Integer(2), new Integer(1), new Integer(2)};
-        System.out.println("Unsorted raw: " + arrayToRawString(unsorted));
+        System.out.println("Unsorted: " + Arrays.toString(unsorted));
+        System.out.println("Unsorted raw: " + arrayToRawString(unsorted) + "\n");
+
         final Integer[] sorted = sort(unsorted, 3);
-        System.out.println("Sorted raw: " + arrayToRawString(sorted));
         System.out.println("Sorted: " + Arrays.toString(sorted));
+        System.out.println("Sorted raw: " + arrayToRawString(sorted));
     }
 
     public static Integer[] sort(Integer[] unsorted, final int maxValue) {
@@ -26,7 +29,6 @@ public class StableCountingSort {
         for (int i = 1; i < maxValue; i++) {
             counters[i] = counters[i] + counters[i - 1];
         }
-
         final Integer[] result = new Integer[unsorted.length];
         for (int j = unsorted.length - 1; j >= 0; j--) {
             final Integer value = unsorted[j];
