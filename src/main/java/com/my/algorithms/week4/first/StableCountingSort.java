c21794dbbcf39b591008cbb1905a0035de4c6405
Dmitry Palaznik <dpalaznik@gmail.com>
2015-02-09 12:13:40 +0300

    Stable counting sort algorithm implementation

--- src/main/java/com/my/algorithms/week4/first/StableCountingSort.java
+++ src/main/java/com/my/algorithms/week4/first/StableCountingSort.java
@@ -3,17 +3,45 @@ package com.my.algorithms.week4.first;
 import java.util.Arrays;
 
 /**
- * Stable counting sort algorithm implementation
+ * Stable counting sort algorithm implementation that doesn't change the order of equaled elements.
  */
 public class StableCountingSort {
 
     public static void main(String[] args) {
-        final int[] unsorted = {2, 3, 2, 1, 2};
-        sort(unsorted);
-        System.out.println(Arrays.toString(unsorted));
+        final Integer[] unsorted = {new Integer(2), new Integer(3), new Integer(2), new Integer(1), new Integer(2)};
+        System.out.println("Unsorted raw: " + arrayToRawString(unsorted));
+        final Integer[] sorted = sort(unsorted, 3);
+        System.out.println("Sorted raw: " + arrayToRawString(sorted));
+        System.out.println("Sorted: " + Arrays.toString(sorted));
     }
 
-    public static void sort(final int[] unsortedValues) {
-        // TODO implement me
+    public static Integer[] sort(Integer[] unsorted, final int maxValue) {
+        final int[] counters = new int[maxValue];
+        for (int i = 0; i < maxValue; i++) {
+            counters[i] = 0;
+        }
+        for (final int i : unsorted) {
+            counters[i - 1] = counters[i - 1] + 1;
+        }
+        for (int i = 1; i < maxValue; i++) {
+            counters[i] = counters[i] + counters[i - 1];
+        }
+
+        final Integer[] result = new Integer[unsorted.length];
+        for (int j = unsorted.length - 1; j >= 0; j--) {
+            final Integer value = unsorted[j];
+            result[counters[value - 1] - 1] = value;
+            counters[value - 1] = counters[value - 1] - 1; // TODO refactor me
+        }
+        return result;
+    }
+
+    private static String arrayToRawString(final Integer[] values) {
+        final StringBuilder result = new StringBuilder("[");
+        for (final Integer value : values) {
+            result.append(value).append(":").append(System.identityHashCode(value)).append(", ");
+        }
+        result.delete(result.length() - 2, result.length());
+        return result.append("]").toString();
     }
 }
