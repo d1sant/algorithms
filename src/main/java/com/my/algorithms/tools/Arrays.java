0341869349f76745162b0e22f69b3db8f7739f60
Dmitry Palaznik <dpalaznik@gmail.com>
2015-02-12 12:46:15 +0300

    Added implementation of merge method

--- src/main/java/com/my/algorithms/tools/Arrays.java
+++ src/main/java/com/my/algorithms/tools/Arrays.java
@@ -7,4 +7,23 @@ public class Arrays {
         array[toIndex] = array[fromIndex];
         array[fromIndex] = to;
     }
+
+    public static int[] merge(final int[] first, final int[] second) {
+        final int[] result = new int[first.length + second.length];
+        int i = 0, j = 0, k = 0;
+        while (i < first.length && j < second.length) {
+            result[k++] = first[i] < second[j] ? first[i++] : second[j++];
+        }
+        while (i < first.length) {
+            result[k++] = first[i++];
+        }
+        while (j < second.length) {
+            result[k++] = second[j++];
+        }
+        return result;
+    }
+
+    public static void merge(final int[] first, final int[] second, final int[] dest, final int destIndex) {
+        System.arraycopy(merge(first, second), 0, dest, destIndex, first.length + second.length);
+    }
 }
--- src/main/java/com/my/algorithms/week4/third/MergeSort.java
+++ src/main/java/com/my/algorithms/week4/third/MergeSort.java
@@ -1,6 +1,7 @@
 package com.my.algorithms.week4.third;
 
 import java.util.Arrays;
+import static com.my.algorithms.tools.Arrays.merge;
 
 /**
  * Merge sort algorithm implementation.
@@ -8,24 +9,39 @@ import java.util.Arrays;
 public class MergeSort {
 
     public static void main(final String[] args) {
+
+        final int[] firstForMerge = {1, 2, 5};
+        final int[] secondForMerge = {3, 4, 6, 8};
+        System.out.println("First array for merge: " + Arrays.toString(firstForMerge));
+        System.out.println("Second array for merge: " + Arrays.toString(secondForMerge));
+        System.out.println("Merged array: " + Arrays.toString(merge(firstForMerge, secondForMerge)) + "\n");
+
+        final int[] merged = new int[firstForMerge.length + secondForMerge.length];
+        merge(firstForMerge, secondForMerge, merged, 0);
+        System.out.println("Merged array (sophisticated): " + Arrays.toString(merged) + "\n");
+
+        System.out.println("Merge of unequaled arrays ({1} and {}): " + Arrays.toString(merge(new int[] {1}, new int[]{})) + "\n");
+
         final int[] unsorted = {2, 3, 2, 1, 2};
+        System.out.println("Initial values: " + Arrays.toString(unsorted));
         sort(unsorted);
-        System.out.println(Arrays.toString(unsorted));
+        System.out.println("Sorted values: " + Arrays.toString(unsorted));
     }
 
-    public static void sort(final int[] unsortedValues) {
-        // TODO Implement me
+    public static void sort(final int[] unsorted) {
+        mergeSort(unsorted, 0, unsorted.length - 1);
     }
 
-    private static int[] mergeSort(final int[] unsorted, final int left, final int right) {
+    private static void mergeSort(final int[] unsorted, final int left, final int right) {
         if (left >= right) {
-            return new int[]{};
+            return;
         }
         final int middle = (left + right) / 2;
-        return merge(mergeSort(unsorted, left, middle), mergeSort(unsorted, middle + 1, right));
+        mergeSort(unsorted, left, middle);
+        mergeSort(unsorted, middle + 1, right);
+        final int[] leftArray = Arrays.copyOfRange(unsorted, left, middle);
+        final int[] rightArray = Arrays.copyOfRange(unsorted, middle + 1, right);
+        merge(leftArray, rightArray, unsorted, left);
     }
 
-    private static int[] merge(final int[] first, final int[] second) {
-        return new int[]{}; // TODO Implement me
-    }
 }
