4f9cd48c2eee678edadd37938b852f9bb7acccbb
Dmitry Palaznik <dpalaznik@gmail.com>
2015-02-12 15:10:46 +0300

    MergeSort: addition logging of algorithm

--- src/main/java/com/my/algorithms/week4/third/MergeSort.java
+++ src/main/java/com/my/algorithms/week4/third/MergeSort.java
@@ -14,13 +14,14 @@ public class MergeSort {
         final int[] secondForMerge = {3, 4, 6, 8};
         System.out.println("First array for merge: " + Arrays.toString(firstForMerge));
         System.out.println("Second array for merge: " + Arrays.toString(secondForMerge));
-        System.out.println("Merged array: " + Arrays.toString(merge(firstForMerge, secondForMerge)) + "\n");
+        System.out.println("Merged array: " + Arrays.toString(merge(firstForMerge, secondForMerge)));
 
         final int[] merged = new int[firstForMerge.length + secondForMerge.length];
         merge(firstForMerge, secondForMerge, merged, 0);
         System.out.println("Merged array (sophisticated): " + Arrays.toString(merged) + "\n");
 
-        System.out.println("Merge of unequaled arrays ({1} and {}): " + Arrays.toString(merge(new int[] {1}, new int[]{})) + "\n");
+        System.out.println("Merge of unequaled arrays ({1} and {}): " + Arrays.toString(merge(new int[] {1}, new int[]{})));
+        System.out.println("Merge of simple arrays ({2} and {1}): " + Arrays.toString(merge(new int[] {2}, new int[]{1})) + "\n");
 
         final int[] unsorted = {2, 3, 2, 1, 2};
         System.out.println("Initial values: " + Arrays.toString(unsorted));
@@ -33,6 +34,7 @@ public class MergeSort {
     }
 
     private static void mergeSort(final int[] unsorted, final int left, final int right) {
+        System.out.println(left + ", " + right);
         if (left >= right) {
             return;
         }
@@ -41,6 +43,7 @@ public class MergeSort {
         mergeSort(unsorted, middle + 1, right);
         final int[] leftArray = Arrays.copyOfRange(unsorted, left, middle);
         final int[] rightArray = Arrays.copyOfRange(unsorted, middle + 1, right);
+        System.out.println("For merge: " + Arrays.toString(leftArray) + " : " + Arrays.toString(rightArray));
         merge(leftArray, rightArray, unsorted, left);
     }
 
