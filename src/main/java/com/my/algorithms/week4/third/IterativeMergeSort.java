6a0b40db160bb1d4308a2d1fcf02942b22ef6f69
Dmitry Palaznik <dpalaznik@gmail.com>
2015-02-20 16:29:48 +0300

    IterativeMergeSort algorithm implementation

--- src/main/java/com/my/algorithms/week4/third/IterativeMergeSort.java
+++ src/main/java/com/my/algorithms/week4/third/IterativeMergeSort.java
@@ -1,6 +1,10 @@
 package com.my.algorithms.week4.third;
 
 import java.util.Arrays;
+import java.util.Deque;
+import java.util.LinkedList;
+
+import static com.my.algorithms.tools.Arrays.merge;
 
 /**
  * Iterative merge sort algorithm implementation.
@@ -9,11 +13,19 @@ public class IterativeMergeSort {
 
     public static void main(final String[] args) {
         final int[] unsorted = {2, 3, 2, 1, 2};
+        System.out.println("Initial values: " + Arrays.toString(unsorted));
         sort(unsorted);
-        System.out.println(Arrays.toString(unsorted));
+        System.out.println("Sorted values: " + Arrays.toString(unsorted));
     }
 
-    public static void sort(final int[] unsortedValues) {
-        // TODO Implement me
+    public static void sort(final int[] unsorted) {
+        final Deque<int[]> queue = new LinkedList<int[]>();
+        for (int i = 0; i < unsorted.length; i++) {
+            queue.offer(Arrays.copyOfRange(unsorted, i, i + 1));
+        }
+        while (queue.size() > 1) {
+            queue.offer(merge(queue.poll(), queue.poll()));
+        }
+        System.arraycopy(queue.poll(), 0, unsorted, 0, unsorted.length);
     }
 }
---
+++ src/main/java/com/my/algorithms/week4/third/MergeSortTask1.java
@@ -0,0 +1,4 @@
+package com.my.algorithms.week4.third;
+
+public class MergeSortTask1 {
+}
