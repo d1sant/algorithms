cee1fd07ce4f67d5f96b38099e49164ad7340fc1
Dmitry Palaznik <dpalaznik@gmail.com>
2015-02-27 18:50:23 +0300

    Fixed Depth First Search Task 2

--- src/main/java/com/my/algorithms/week5/second/DepthFirstSearchTask2.java
+++ src/main/java/com/my/algorithms/week5/second/DepthFirstSearchTask2.java
@@ -13,38 +13,30 @@ public class DepthFirstSearchTask2 {
         final int[][] graph = new int[vertexes][];
         scanner.nextLine();
         for (int index = 0; index < edges; index++) {
-            add(graph, scanner.nextInt(), scanner.nextInt());
+            addUndirected(graph, scanner.nextInt(), scanner.nextInt());
         }
         System.out.println(new HashSet<Integer>(Arrays.asList(dfs(graph))).size());
     }
 
-    private static void add(final int[][] graph, final int firstVertex, final int secondVertex) {
+    private static void addUndirected(final int[][] graph, final int firstVertex, final int secondVertex) {
         addEdge(graph, firstVertex - 1, secondVertex - 1);
+        addEdge(graph, secondVertex - 1, firstVertex - 1);
     }
 
-    private static void addEdge(final int[][] graph, final int firstVertex, final int secondVertex) {
-        final int vertex;
-        final int vertexForAdd;
-        if (firstVertex < secondVertex) {
-            vertex = firstVertex;
-            vertexForAdd = secondVertex;
-        } else {
-            vertex = secondVertex;
-            vertexForAdd = firstVertex;
-        }
-        if (graph[vertex] != null) {
-            int length = graph[vertex].length;
+    private static void addEdge(final int[][] graph, final int vertexFrom, final int vertexTo) {
+        if (graph[vertexFrom] != null) {
+            int length = graph[vertexFrom].length;
             for (int edgeIndex = 0; edgeIndex < length; edgeIndex++) {
-                if (graph[vertex][edgeIndex] == vertexForAdd) {
+                if (graph[vertexFrom][edgeIndex] == vertexTo) {
                     return;
                 }
             }
             final int[] edges = new int[length + 1];
-            System.arraycopy(graph[vertex], 0, edges, 0, length);
-            edges[length] = vertexForAdd;
-            graph[vertex] = edges;
+            System.arraycopy(graph[vertexFrom], 0, edges, 0, length);
+            edges[length] = vertexTo;
+            graph[vertexFrom] = edges;
         } else {
-            graph[vertex] = new int[]{vertexForAdd};
+            graph[vertexFrom] = new int[]{vertexTo};
         }
     }
 
@@ -63,7 +55,6 @@ public class DepthFirstSearchTask2 {
             cComponent++;
         }
     }
-
 
     private static void explore(final int[][] graph, final int vertex, final Integer[] visited, final int cComponent) {
         visited[vertex] = cComponent;
